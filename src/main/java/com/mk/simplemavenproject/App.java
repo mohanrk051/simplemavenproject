beb9bbf551e8f2125b2a9ab4a393ef7f99cac5ad
mohan <mohan@gmail.com>
2021-07-15 13:22:35 +0530

    added new feature branch

--- src/main/java/com/mk/simplemavenproject/App.java
+++ src/main/java/com/mk/simplemavenproject/App.java
@@ -8,13 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("new feature is added");
-        System.out.println("new feature 2 is added");
-        System.out.println("new feature 3 is added in develop");
-        System.out.println("new feature 4 is added");
-        System.out.println("developed latest update from develop branch");
-
-         System.out.println("feature branch added on 15th july");
+        System.out.println( "welcome feautre branch" );
+        
     }
 }
