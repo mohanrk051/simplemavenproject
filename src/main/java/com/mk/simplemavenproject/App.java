dc9c75b681a06a7a12cec2d100ab39dd49a326d7
mohan <mohan@gmail.com>
2021-07-15 12:17:30 +0530

    latest update from feature branch  on 15th july added

--- src/main/java/com/mk/simplemavenproject/App.java
+++ src/main/java/com/mk/simplemavenproject/App.java
@@ -11,7 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("new feature is added");
         System.out.println("new feature 2 is added");
-        
-        System.out.println("new feature 4 is added");
+         System.out.println("new feature 4 is added");
+         System.out.println("feature branch added on 15th july");
     }
 }
