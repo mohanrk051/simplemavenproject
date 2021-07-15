1a81e6a6f26065aeafd1fd68b84e9824fb36e2b8
mohan <mohan@gmail.com>
2021-07-15 13:45:05 +0530

    added new feature branch

--- src/main/java/com/mk/simplemavenproject/App.java
+++ src/main/java/com/mk/simplemavenproject/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "welcome feautre branch" );
-        
+        System.out.println("nre feature15th july");
     }
 }
